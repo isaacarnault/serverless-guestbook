03a2368bb0338fe5326fed5b2d3de1414d64a97e
Isaac <44641020+isaacarnault@users.noreply.github.com>
2022-07-18 02:06:40 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://63aa9d6e.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
